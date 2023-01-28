751c535ff7eef7bd09f986c81239cff253970586
Gion <69393137+gionsunday@users.noreply.github.com>
2023-01-28 22:09:27 +0100

    Add files via upload

---
+++ models/message.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+const messageSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        trim:true,
+        minlength:[3, "name must not be less than 3 characters"],
+        required: [true, 'please provide an name'],
+        maxlength: [50, "name must not be greater than 20 characters"]
+    }, 
+    email:{
+        type: String,
+        unique:true,
+        trim:true,
+        required: [true, 'please provide an name'],
+        maxlength: [50, "name must not be greater than 20 characters"]
+    }, 
+    message:{
+        type: String,
+        trim:true,
+        required: [true, 'please provide an name'],
+        maxlength: [50, "name must not be greater than 20 characters"]
+    }, 
+    })
+
+
+module.exports = mongoose.model('Message', messageSchema)
+
+
+
+
+
+
+
+
+
+
+/*
+
+*/
